937e549da4c9712b9f6c4a1a50466291b981e075
fullname <email>
2023-09-27 11:05:01 +0100

    Add index route

--- server/helpers/createRouter.js
+++ server/helpers/createRouter.js
@@ -3,6 +3,13 @@ const express = require("express");
 function createRouter(collection) {
   const router = express.Router();
 
+  router.get("/", (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs));
+  });
+
   return router;
 }
 
